d5603ebccb45d8e0c6ea8968456beac713e4fcc7
Evgeny Chesnokov <chesnokov.evgeniy@otr.ru>
2013-12-21 21:39:58 +0700

    initial project structure setup

---
+++ projects/mmorpg-server/src/main/java/com/elvin/mmorpg/server/App.java
@@ -0,0 +1,13 @@
+package com.elvin.mmorpg.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
